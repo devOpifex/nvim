0e1e158016d3cd341ce8d3b6c5d4dbde63069086
John Coene <jcoenep@gmail.com>
2026-02-21 22:25:59 +0100

    fix: telescope shortcut

--- init.lua
+++ init.lua
@@ -61,6 +61,7 @@ require("lazy").setup({
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 			vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 		end,
 		dependencies = {
 			"nvim-lua/plenary.nvim",
